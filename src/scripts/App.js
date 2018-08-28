5a118691346883aa15ed4e4e4d43311a8edb2238
Shangni <shangnihu@gmail.com>
2018-08-28 19:01:39 -0400

    Initial commit, with minimal set up

---
+++ src/scripts/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import '../styles/styles.css';
+
+// urls/keys for Yandex API
+const translateURL = "https://translate.yandex.net/api/v1.5/tr.json/translate";
+const languagesURL = 'https://translate.yandex.net/api/v1.5/tr.json/getLangs';
+const detectURL = 'https://translate.yandex.net/api/v1.5/tr.json/detect';
+const apiKey = "trnsl.1.1.20180828T150321Z.763350fbc08abff9.2cddae20856c6abb37452b3c5e6fc8c12805a059";
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      langs: {},
+      langToTranslate: "",
+      textToTranslate: ""
+    }
+  }
+
+  componentDidMount() {
+    console.log('component did mount called');
+    axios.get(languagesURL, {
+      params: {
+        key: apiKey,
+        ui: 'en'
+      }
+    }).then((res) => {
+      console.log(res.data.langs);
+      this.setState({
+        langs: res.data.langs
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        <header className="landing">
+          <h1 className="logo">Phrasebook</h1>
+
+        </header>
+
+      </div>
+    );
+  }
+}
+
+export default App;
